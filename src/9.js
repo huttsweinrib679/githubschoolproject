8448af12ab08044009d3ed155dbddccde9c4b219
huttsweinrib679 <huttsweinrib679@gmail.com>
2025-03-14 10:44:59 +0000

    Add 9.js

---
+++ src/9.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    res.send('Hello World!');
+  } else {
+    res.send(`Hello ${name}!`);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
